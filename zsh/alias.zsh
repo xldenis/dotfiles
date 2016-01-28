54d9f008640a4507ab00ff85b8477eb83b55926d
Xavier Denis <xldenis@gmail.com>
2016-01-28 12:40:54 -0500

    turn on vim mode

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -7,3 +7,10 @@ alias c=projects
 export DIRRSTACKSIZE=8
 setopt autopushd pushdminus pushdsilent pushdtohome
 alias pd=popd
+
+function vss() {
+ prev_dir=$(pwd)
+ c shopify/vagrant
+ vagrant ssh
+ cd $prev_dir
+}
